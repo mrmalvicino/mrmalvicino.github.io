75b584177b62cbfcbbc5c0da6c629bf6943e626e
mrmalvicino <mrmalvicino@gmail.com>
2024-05-06 04:12:01 -0300

    Bug fix

--- script.js
+++ script.js
@@ -44,11 +44,11 @@ document.addEventListener("DOMContentLoaded", function()
 
         if (selectedLanguage == "english")
         {
-            text = "Hello! My name is Maximiliano Raúl Malvicino. I am passionate about acoustics and programming. Throughout my career, I have been working on projects that combine these fields and converge in signals processing. Until now, my work experience has been mostly focused on teaching. Motivated by the new challenges that the growth of the technology industry implies, I am currently looking for my first experience in the IT field as a software developer.";
+            text = "Hi! I am Max, and I am passionate about acoustics and programming. Throughout my career, I have been working on projects that combine these fields and converge in signals processing. Until now, my work experience has been mostly focused on teaching. Motivated by the new challenges that the growth of the technology industry implies, I am currently looking for my first experience in the IT field as a software developer.";
         }
         else if (selectedLanguage == "spanish")
         {
-            text = "¡Hola! Mi nombre es Maximiliano Raúl Malvicino. Me apasiona la acústica y la programación. A lo largo de mi carrera trabajé en proyectos que combinan estos campos y convergen en el procesamiento de señales. Hasta ahora mi experiencia laboral se centró mayoritariamente en la docencia. Motivado por los nuevos desafíos que implica el crecimiento de la industria tecnológica, actualmente estoy buscando mi primera experiencia en el campo de las TI como desarrollador de software.";
+            text = "¡Hola! Soy Maxi, y me apasiona la acústica y la programación. A lo largo de mi carrera trabajé en proyectos que combinan estos campos y convergen en el procesamiento de señales. Hasta ahora mi experiencia laboral se centró mayoritariamente en la docencia. Motivado por los nuevos desafíos que implica el crecimiento de la industria tecnológica, actualmente estoy buscando mi primera experiencia en el campo de las TI como desarrollador de software.";
         }
 
         presentationParagraph.textContent = text;
