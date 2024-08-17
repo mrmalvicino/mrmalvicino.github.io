c3e3958e6ba09388be7160e1da3bfd3cc7510e53
mrmalvicino <mrmalvicino@gmail.com>
2024-08-17 00:00:13 -0300

    Buttons translation

--- script/translation_index.js
+++ script/translation_index.js
@@ -5,6 +5,8 @@ document.addEventListener("DOMContentLoaded",
         var presentationTxt = document.getElementById("presentationTxt");
         var resumeBtn = document.getElementById("resumeBtn");
         var sourceCodeBtns = document.getElementsByName('sourceCodeBtn');
+        var downloadBinaryBtns = document.getElementsByName('downloadBinaryBtn');
+        var openOnlineBtns = document.getElementsByName('openOnlineBtn');
 
         function getSelectedLanguage()
         {
@@ -78,12 +80,52 @@ document.addEventListener("DOMContentLoaded",
             }
         }
 
+        function translateDownloadBinaryBtns(selectedLanguage)
+        {
+            var text = "";
+
+            if (selectedLanguage == "english")
+            {
+                text = "Download";
+            }
+            else if (selectedLanguage == "spanish")
+            {
+                text = "Descargar";
+            }
+
+            for (var i = 0; i < downloadBinaryBtns.length; i++)
+            {
+                downloadBinaryBtns[i].textContent = text;
+            }
+        }
+
+        function translateOpenOnlineBtns(selectedLanguage)
+        {
+            var text = "";
+
+            if (selectedLanguage == "english")
+            {
+                text = "Open Online";
+            }
+            else if (selectedLanguage == "spanish")
+            {
+                text = "Abrir en línea";
+            }
+
+            for (var i = 0; i < openOnlineBtns.length; i++)
+            {
+                openOnlineBtns[i].textContent = text;
+            }
+        }
+
         function translate()
         {
             var selectedLanguage = getSelectedLanguage();
             translatePresentationTxt(selectedLanguage);
             translateResumeBtn(selectedLanguage);
             translateSourceCodeBtns(selectedLanguage);
+            translateDownloadBinaryBtns(selectedLanguage);
+            translateOpenOnlineBtns(selectedLanguage);
         }
 
         translate();
